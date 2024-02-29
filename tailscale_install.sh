5b10945b705f5efa4797896ffaf1028ca8058745
Mickael Porte <mickael.porte@cecielec.fr>
2024-02-29 15:36:56 +0100

    fix symbolic links

--- tailscale_install.sh
+++ tailscale_install.sh
@@ -8,9 +8,6 @@ gateway_model_name=$(uci show einfo.dev.name | grep -o "'[^']*'" | sed "s/'//g")
 gateway_eui=$(uci show einfo.dev.gw_eui | grep -o "'[^']*'" | sed "s/'//g")
 gateway_hostname=$HOSTNAME
 
-echo "gateway hostname $gateway_hostname"
-echo "gateway eui $gateway_eui"
-echo "gateway model name $gateway_model_name"
 
 # When the model name not found using uci, it means that it should be a RAK7289C or RAK7249
 # Check the model name using einfo
@@ -18,6 +15,10 @@ if [ -z "$gateway_model_name" ]; then
     gateway_model_name="$(einfo show | grep DEV_NAME | sed 's/.*"\([^"]*\)".*/\1/')"
     gateway_eui="$(einfo show | grep GATEWAY_EUI | sed 's/.*"\([^"]*\)".*/\1/')"
 fi
+
+echo "gateway hostname $gateway_hostname"
+echo "gateway eui $gateway_eui"
+echo "gateway model name $gateway_model_name"
 
 if [[ "$gateway_model_name" == "RAK7289C" || "$gateway_model_name" == "RAK7249" ]]; then
     echo "Using RAMIPS architecture and using sd card to store the tailscale"
@@ -31,9 +32,6 @@ else
     echo "Invalid model name (GATEWAY_MODEL) $gateway_model_name"
     exit 1
 fi
-
-
-
 
 echo "Downloading packet $PACKET_NAME"
 wget -P /mnt/mmcblk0p1/tailscale $DOWNLOAD_SERVER/$PACKET_NAME
@@ -49,12 +47,12 @@ opkg install /mnt/mmcblk0p1/tailscale/$TAILSCALE_PACKET_NAME
 rm /mnt/mmcblk0p1/tailscale/$TAILSCALE_PACKET_NAME
 
 # As side effect the packet installation will create
-# some files for /etc/init.d/tailscale and /etc/init.d/tailscaled
-echo "Remove initials /etc/init.d/tailscale[d]"
-rm /etc/init.d/tailscale
-rm /etc/init.d/tailscaled
+# some files for /usr/bin/tailscale and  /usr/bin/tailscaled
+echo "Remove initial files /usr/bin/tailscale[d]"
+rm /usr/bin/tailscale
+rm /usr/bin/tailscaled
 
-ehco "Linking binaries to $TAILSCALE_BINARY_PATH/$TAILSCALE_BINARY_NAME"
+echo "Linking binaries to $TAILSCALE_BINARY_PATH/$TAILSCALE_BINARY_NAME"
 ln -s "$TAILSCALE_BINARY_PATH/$TAILSCALE_BINARY_NAME" /usr/sbin/tailscaled
 ln -s "$TAILSCALE_BINARY_PATH/$TAILSCALE_BINARY_NAME" /usr/sbin/tailscale
 
